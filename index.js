a9cf98857c05e06dca4259dc19884f9df6de2211
Brian Lambert <brianlambert2408@gmail.com>
2022-08-26 15:24:20 +1000

    commit tree 78d51220d0e87cc6809181887adb6838214dbf65

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(arrNames, eventName){
+
+    const msgs = [];
+
+    for(let c = 0; c < arrNames.length; c++){
+        msgs.push(`Thank you, ${arrNames[c]}, for the wonderful ${eventName} gift!`);
+    }
+
+    return msgs;
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+}
